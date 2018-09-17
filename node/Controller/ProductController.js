2ab6ee94a5969b34da46a6e707d5226f22c6b969
Boi <you@example.com>
2018-09-18 07:44:28 +1200

    Removed mailgun credentials in order to comply with public sharing

--- node/Controller/ProductController.js
+++ node/Controller/ProductController.js
@@ -4,13 +4,14 @@ var router = express.Router();
 var connection = require("../connection/connect")(); 
 
 // MailGun import
-var api_key = '4a94b9ddc7e28006170e29b9e7ab5a0b-8889127d-d9d89be0';
+var api_key = '';
 var domain = 'sandboxdc705a9195684b13948ef2946002cf14.mailgun.org';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 var routes = function (){
-    //default route of the product controller file
-    router.route('/gettrees')
+
+
+    router.route('/getitems')
         .get(function (req, res){
             // Perform a SELECT * query from the Trees table
             connection.query('SELECT * FROM items', function (error, results, fields) {
@@ -20,24 +21,34 @@ var routes = function (){
                     throw error;
 
                 // Prints the query result in JSON Format
-                res.json(results); 
+                res.json(results[0].price); 
             });
         });
-    router.route('/removeproduct')
+
+
+
+    // ------------ SHOPPING CART API CALLS -------------------------- 
+
+    // Removes all of a particular product from users shopping cart
+    router.route('/removeproduct/:item')
         .post(function (req, res){
             try {
-                res.status(200).json("Product: Pine has been removed on cart: 4521"); 
-                // });
+                connection.query('DELETE FROM `'+req.body.cart+'` WHERE itemName LIKE "'+req.params.item+'"', function (error, results, fields) {
+                    res.status(200).json("Product: "+req.params.item+" has been removed on cart: "+req.body.cart); 
+                });
             } catch (error) {
                 res.status(200).json("Something went wrong with removing your product from cart");
             }   
         });
 
-    router.route('/addproduct')
+    // Adds product to users shopping cart
+    router.route('/addproduct/:item')
         .post(function (req, res){
             try {
-                res.status(200).json("Product: Apple Sapling has been added to cart: 4521"); 
-                // });
+                connection.query('INSERT INTO `'+req.body.cart+'`(`itemName`, `price`, `Deliverable`, `dateAdded`, `purchased`) VALUES ("'
+                +req.params.item+'",'+req.body.price+', '+req.body.deliverable+', "'+new Date()+'", "N")', function (error, results, fields) {
+                    res.status(200).json("Product: "+req.params.item+" has been added to cart: "+req.body.cart); 
+                });
             } catch (error) {
                 res.status(200).json("Something went wrong with adding your product to cart");
             }   
