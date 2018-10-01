efa38065ae51250df2229756ff9dc6e030d284dc
Boi <you@example.com>
2018-10-01 15:44:08 +1300

    added frontend login functionality

--- my-app/src/components/ProfileMenu/ProfileMenu.jsx
+++ my-app/src/components/ProfileMenu/ProfileMenu.jsx
@@ -3,6 +3,33 @@ import "./ProfileMenu.css";
 import ClearIcon from "@material-ui/icons/Clear";
 
 export default class ProfileMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: '',
+      numberOfGuests: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit =  this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    alert('Username entered: ' + this.state.isGoing + ", Password Entered: "+this.state.numberOfGuests);
+    event.preventDefault();
+  }
+
+  // Input change to handle form changes
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     var visibility = "hide";
 
@@ -15,7 +42,29 @@ export default class ProfileMenu extends Component {
         <ClearIcon onMouseDown={this.props.handleMouseDown} />
         {this.props.children}
         <br/>
-        <b>Account login and settings section</b>
+        <b>Account</b>
+        <br/>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+          Username:
+            <input
+              name="isGoing"
+              type="text"
+              checked={this.state.isGoing}
+              onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              name="numberOfGuests"
+              type="password"
+              value={this.state.numberOfGuests}
+              onChange={this.handleInputChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
